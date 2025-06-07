a8c2d3625f6446bb966b0143f3f7a41e93795de3
Betero Tiikana <btiikana@gmail.com>
2025-06-07 00:11:32 -1000

    06072025 --> Completed Homework - ObjectsLab

--- JAVA/homework/June05objects/MainPyramid.java
+++ JAVA/homework/June05objects/MainPyramid.java
@@ -4,88 +4,104 @@ import java.awt.Graphics;
 import java.awt.Color;
 import java.awt.Point;
 
+//Name: Betero Tiikana
+//Course: CS202
+
 public class MainPyramid extends JPanel {
 
-    private Pyramid pyramid;
-    private Pyramid secondPyramid;
+    //FIELDS - Main section for object declaration
+    private Pyramid pyramid;// reference variable (1st pyramid)
+    private Pyramid secondPyramid;// reference variable (2nd pyramid)
 
+    //CONSTRUCTOR - Main section
     public MainPyramid() {
-        //call first pyramid
+        // Instantiate and setup our first pyramid
         pyramid = new Pyramid(100, 200);
         pyramid.setBrickColor(Color.ORANGE);
         pyramid.setStairColor(Color.BLACK);
-
-        //call second pyramid
+        
+        // Instantiate and setup our second pyramid
         secondPyramid = new Pyramid(new Point(250, 100));
         secondPyramid.setBrickColor(Color.BLACK);
         secondPyramid.setStairColor(Color.WHITE);
         secondPyramid.setStairSpacing(13);
     }
 
+    //DRAWING BACKGROUND ELEMENTS - Optional section
     public void backgroundItems(Graphics g) {
-    // Background
+        // Sky
         g.setColor(Color.BLUE);
         g.fillRect(0, 0, getWidth(), getHeight());
-    // Sun
+
+        // Sun
         g.setColor(Color.YELLOW);
         g.fillOval(20, 20, 60, 60);
-    // Cloud 1
+
+        // Cloud 1
         g.setColor(Color.WHITE);
         g.fillOval(100, 10, 50, 20);
         g.fillOval(120, 10, 50, 10);
-    // Cloud 2
+
+        // Cloud 2
         g.fillOval(200, 10, 50, 20);
         g.fillOval(220, 10, 50, 10);
-    // Cloud 3
+
+        // Cloud 3
         g.fillOval(300, 10, 50, 20);
         g.fillOval(320, 10, 50, 10);
-    // Mountains
+
+        // Mountains
         g.setColor(Color.GREEN);
         g.fillRect(0, 170, getWidth(), getHeight());
         g.fillOval(-20, 120, 180, 100);
 
-    // Vegetation
+        // Vegetation
         g.fillRect(200, 150, 20, 70);
         g.fillRect(250, 150, 20, 70);
         g.fillRect(300, 150, 20, 70);
         g.fillRect(350, 150, 20, 70);
-    //Airplane
+
+        // Airplane
         g.setColor(Color.WHITE);
         g.fillRect(250, 70, 100, 20);
-        g.fillRect(240, 60, 10,20);
-        g.fillRect(289, 60, 10,20);
+        g.fillRect(240, 60, 10, 20);
+        g.fillRect(289, 60, 10, 20);
         g.setColor(Color.ORANGE);
-        g.fillRect(350, 70, 10,20);
-    // Trail and Seasider
+        g.fillRect(350, 70, 10, 20);
+
+        // Trail and "GO SEASIDER!!" Banner
         g.setColor(Color.WHITE);
-        g.fillRect(200, 70, 5,5);
-        g.fillRect(210, 70, 5,5);
-        g.fillRect(220, 70, 5,5);
-        g.fillRect(230, 70, 5,5);
-        g.fillRect(100, 70, 100,40);
+        g.fillRect(200, 70, 5, 5);
+        g.fillRect(210, 70, 5, 5);
+        g.fillRect(220, 70, 5, 5);
+        g.fillRect(230, 70, 5, 5);
+        g.fillRect(100, 70, 100, 40);
         g.setColor(Color.RED);
         g.drawString("GO SEASIDER!!", 105, 90);
-    // Ground
+
+        // Ground
         g.setColor(Color.LIGHT_GRAY);
         g.fillRect(0, 300, getWidth(), getWidth());
 
-    // My name
+        // Signature / Name tag
         g.setColor(Color.WHITE);
         g.fillRect(150, 320, 80, 35);
         g.setColor(Color.BLACK);
         g.drawString("Betero Tiikana", 150, 340);
     }
 
+    //Main section for drawing
     @Override
     public void paintComponent(Graphics g) {
-        //call backgroundItems method
+        // Draw background and environment
         backgroundItems(g);
-        
-        //call draw method
+
+        // Draw both pyramids
         pyramid.draw(g);
         secondPyramid.draw(g);
     }
 
+    // MAIN METHOD - Main section for entire program
     public static void main(String[] args) {
         var window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- JAVA/homework/June05objects/Pyramid.java
+++ JAVA/homework/June05objects/Pyramid.java
@@ -1,35 +1,37 @@
 import java.awt.*;
 
+//Name: Betero Tiikana
+//Course: CS202
+
 public class Pyramid {
 
+    //Fields
     private Point pos;
     private Color stairColor;
     private Color brickColor;
-    private int spacing = 10; // default spacing
+    private int spacing = 10;
 
-    // default constructor
+    //Default Constructor for 
     public Pyramid() {
-        pos = new Point();
-        this.pos.x = 100;
-        this.pos.y = 200;
-        stairColor = Color.BLACK;
-        brickColor = Color.ORANGE;
+        this(new Point(100, 200));//copy x and y logic
+        //inherits default colors too from x and y constructor
     }
 
-    // x and y constructor
+    //x and y constructor for second pyramid position
     public Pyramid(int x, int y) {
-        pos = new Point(x, y);
+        pos = new Point(x, y);// main focus for pyramid location
         stairColor = Color.BLACK;
         brickColor = Color.ORANGE;
     }
 
     // Point constructor
     public Pyramid(Point p) {
-        pos = p;
+        pos = p;// Set position using Point object
         stairColor = Color.BLACK;
         brickColor = Color.ORANGE;
     }
 
+    // setters
     public void setStairColor(Color c) {
         stairColor = c;
     }
@@ -39,13 +41,15 @@ public class Pyramid {
     }
 
     public void setStairSpacing(int space) {
-        this.spacing = space;
+        this.spacing = space;// Set custom spacing between stair lines
     }
 
+    //Draw Method
     public void draw(Graphics g) {
         int x = pos.x;
         int y = pos.y;
 
+        // Draw bricks
         g.setColor(brickColor);
         g.fillRect(x + 40, y, 90, 40);
         g.fillRect(x + 20, y + 30, 130, 60);
@@ -53,10 +57,12 @@ public class Pyramid {
         g.fillRect(x - 20, y + 70, 210, 50);
         g.fillRect(x - 40, y + 90, 250, 40);
 
+        // Draw front stairs(lines)
         g.setColor(stairColor);
-        g.drawOval(x + 48, y + 5, 75, 30);
+        g.drawOval(x + 48, y + 5, 75, 30); // top oval/stair
 
-        g.drawLine(x + 80, y + 70 + spacing, x + 100, y + 70 + spacing);
+        // Front stair lines spaced using the spacing value
+        g.drawLine(x + 80, y + 70 + spacing * 0, x + 100, y + 70 + spacing * 0);
         g.drawLine(x + 60, y + 70 + spacing * 1, x + 120, y + 70 + spacing * 1);
         g.drawLine(x + 40, y + 70 + spacing * 2, x + 140, y + 70 + spacing * 2);
         g.drawLine(x + 20, y + 70 + spacing * 3, x + 160, y + 70 + spacing * 3);
